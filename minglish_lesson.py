b89a12c556b9968fb6e6c09ed290b59c0c3a4ab3
Andrew Tan <andrewztan12@gmail.com>
2016-06-12 15:39:39 -0700

    using DFS graph traversal

--- minglish_lesson.py
+++ minglish_lesson.py
@@ -28,96 +28,94 @@ Inputs:
 Output:
     (string) "bac"
 """
-def get_first_letters(words):
-	if len(words) == 0:
-		return []
-	first_letters = []
-	first_letters.append(words[0][0])
-	for i in range(1, len(words)):
-		if words[i][0] != words[i - 1][0]:
-			first_letters.append(words[i][0]) 
-	return first_letters;
+def get_first_letters(w):
+    words = []
+    for i in range(len(w)):
+        if len(w[i]) > 0:
+            words.append(w[i])
+    if len(words) == 0:
+        return []
+    first_letters = []
+    first_letters.append(words[0][0])
+    for i in range(1, len(words)):
+        if words[i][0] != words[i - 1][0]:
+            first_letters.append(words[i][0]) 
+    return first_letters;
 
 # similar to the merge part of merge sort
 def combine(lst1, lst2):
-	# print(lst1)
-	# print(lst2)
-	combined = []
-	i = 0
-	j = 0
-	k = 0
-
-	while i < len(lst1) and j < len(lst2):
-		if lst1[i] in lst2:
-			if lst2.index(lst1[i]) > j:
-				combined.append(lst2[j])
-				j += 1	
-			elif lst2.index(lst1[i]) == j:
-				combined.append(lst1[i])
-				i += 1
-				j += 1
-			else:
-				i += 1
-			k += 1
-		elif lst2[j] in lst1:
-			if lst1.index(lst2[i]) > i:
-				combined.append(lst1[i])
-				i += 1
-			elif lst1.index(lst2[i]) == j:
-				combined.append(lst1[i])
-				i += 1
-				j += 1
-			else:
-				combined.append(lst2[j])
-				j += 1
-			k += 1
-		else:
-			i += 1
-			j += 1
-
-	if i < len(lst1):
-		combined += lst1[i:]
-	if j < len(lst2):
-		combined += lst2[j:]
-	# print(combined)
-	return combined
+    combined = []
+    i = 0
+    j = 0
+    k = 0
+    while i < len(lst1) and j < len(lst2):
+        if lst1[i] in lst2:
+            if lst2.index(lst1[i]) > j:
+                combined.append(lst2[j])
+                j += 1    
+            elif lst2.index(lst1[i]) == j:
+                combined.append(lst1[i])
+                i += 1
+                j += 1
+            else:
+                i += 1
+            k += 1
+        elif lst2[j] in lst1:
+            if lst1.index(lst2[j]) > i:
+                combined.append(lst1[i])
+                i += 1
+            elif lst1.index(lst2[j]) == j:
+                combined.append(lst1[i])
+                i += 1
+                j += 1
+            else:
+                combined.append(lst2[j])
+                j += 1
+            k += 1
+        else:
+            i += 1
+            j += 1
+    if i < len(lst1):
+        combined += lst1[i:]
+    if j < len(lst2):
+        combined += lst2[j:]
+    return combined
 
 def alphabet(words, dictionary):
-	first_letters = get_first_letters(words)
-	alphabet_list = combine(first_letters, dictionary)
-	if len(first_letters) == len(words):
-		return alphabet_list
-	next_words = [words[0][1:]]
-	for i in range(1, len(words)):
-		if len(words[i]) > 1:
-			if words[i][0] == words[i-1][0]:
-				next_words.append(words[i][1:])
-			else:
-				if len(next_words) > 1:
-					alphabet_list = alphabet(next_words, alphabet_list)
-				next_words = [words[i][1:]]
-	# print(next_words)
-	if len(next_words) > 1:
-		# print(1)
-		alphabet_list = alphabet(next_words, alphabet_list)
-	return alphabet_list
+    first_letters = get_first_letters(words)
+    alphabet_list = combine(first_letters, dictionary)
+    if len(first_letters) == len(words):
+        return alphabet_list
+    next_words = [words[0][1:]]
+    for i in range(1, len(words)):
+        if len(words[i]) > 1:
+            if words[i][0] == words[i-1][0]:
+                next_words.append(words[i][1:])
+            else:
+                if len(next_words) > 1:
+                    alphabet_list = alphabet(next_words, alphabet_list)
+                next_words = [words[i][1:]]
+    if len(next_words) > 1:
+        alphabet_list = alphabet(next_words, alphabet_list)
+    return alphabet_list
 
 def answer(words):
-	alphabet_list = alphabet(words, [])
-	alphabet_string = ""
-	for letter in alphabet_list:
-		alphabet_string += letter
-	return alphabet_string
+    alphabet_list = alphabet(words, [])
+    alphabet_string = ""
+    for letter in alphabet_list:
+        alphabet_string += letter
+    return alphabet_string
 
 
 def test():
-	words = ["z", "yx", "yz"]
-	# print(answer(words))
-	assert answer(words) == "xzy"
-	words = ["y", "z", "xy"]
-	assert answer(words) == "yzx"
-	words = ["ba", "ab", "cb"]
-	assert answer(words) == "bac"
+    words = ["z", "yx", "yz"]
+    assert answer(words) == "xzy"
+    words = ["y", "z", "xy"]
+    assert answer(words) == "yzx"
+    words = ["ba", "ab", "cb"]
+    assert answer(words) == "bac"
+    words = ["abc", "acd", "bcc", "bcd"]
+    assert answer(words) == "abcd"
 
 test()
 
---
+++ minglish_lesson2.py
@@ -0,0 +1,75 @@
+def answer(words):
+    # print(words)
+    graph = make_graph(words)
+    # print(graph)
+    start = start_vertex(graph)
+    # print(start)
+
+    letters = []
+    seen = []
+
+    # DFS postorder graph traversal
+    def traverse(vertex):
+        if vertex not in seen:
+            seen.append(vertex)
+            if vertex in graph:
+                for edge in graph[vertex]:
+                    traverse(edge)
+            letters.append(vertex)
+
+    traverse(start)
+
+    # topological sort - reverse DFS postorder
+    ordered = letters[::-1]
+    alphabet = stringify(ordered)
+    return alphabet
+
+def stringify(letters):
+    alphabet = ''
+    for letter in letters:
+        alphabet += letter
+    # print(alphabet)
+    return alphabet
+
+def make_graph(words):
+    graph = {}
+    for i in range(1, len(words)):
+        pair = find_edge(words[i-1], words[i])
+        # print(pair)
+        if pair is not None:
+            vertex, edge = pair
+            if vertex in graph:
+                # add edge to vertex
+                graph[vertex].append(edge)
+            else:
+                # create vertex, edge pair
+                graph[vertex] = [edge]
+    return graph
+
+
+def find_edge(a, b):
+    l = min(len(a), len(b))
+    for i in range(l):
+        if a[i] != b[i]:
+            return a[i], b[i]
+
+def start_vertex(graph):
+    edges = []
+    for values in graph.values():
+        for val in values:
+            edges.append(val)
+    for vertex in graph:
+        if vertex not in edges:
+            return vertex
+
+def test():
+    words = ["z", "yx", "yz"]
+    assert answer(words) == "xzy"
+    words = ["y", "z", "xy"]
+    assert answer(words) == "yzx"
+    words = ["ba", "ab", "cb"]
+    assert answer(words) == "bac"
+    words = ["abc", "acd", "bcc", "bcd"]
+    assert answer(words) == "abcd"
+
+test()
