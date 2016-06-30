a9a6f9732606a8b4c9ad47fa001efcba22d23cb0
andrewztan <atan@atan.local>
2016-06-29 22:00:53 -0700

    Line Up The Captives

--- line_up_the_captives.py
+++ line_up_the_captives.py
@@ -39,12 +39,14 @@ Output:
     (string) "24"
 """
 
+import math.factorial
+
 def answer(x, y, n):
     return str(solve(x, y, n))
 
 def solve(x, y, n):
     if x + y > n + 1 or (x == 1 and y == 1):
-        return 0:
+        return 0
     if x + y == n + 1:
         if x == n or y == n:
             return 1
@@ -53,57 +55,101 @@ def solve(x, y, n):
     a = min(x, y)
     b = max(x, y)
 
-
-
-
+def nCr(n, r):
+    f = factorial
+    return f(n) / f(r) / f(n-r)
 
 
 def test():
     x = 1
     y = 1
     n = 1
-    assert "1" = answer(x, y, n)
+    assert "1" == answer(x, y, n)
     x = 1
     y = 1
     n = 3
-    assert "0" = answer(x, y, n)
+    assert "0" == answer(x, y, n)
     x = 1
     y = 2
     n = 3
-    assert "1" = answer(x, y, n)
+    assert "1" == answer(x, y, n)
     x = 1
     y = 3
     n = 3
-    assert "1" = answer(x, y, n)
+    assert "1" == answer(x, y, n)
     x = 2
     y = 2
     n = 3
-    assert "2" = answer(x, y, n)
+    assert "2" == answer(x, y, n)
     x = 2
     y = 3
     n = 3
-    assert "0" = answer(x, y, n)
+    assert "0" == answer(x, y, n)
     x = 3
     y = 3
     n = 3
-    assert "0" = answer(x, y, n)
+    assert "0" == answer(x, y, n)
     x = 1
     y = 2
     n = 6
-    assert "24" = answer(x, y, n)
+    assert "24" == answer(x, y, n)
     x = 2
     y = 3
     n = 5
-    assert "8" = answer(x, y, n)
+    assert "8" == answer(x, y, n)
     x = 3
     y = 3
     n = 5
-    assert "4" = answer(x, y, n)
+    assert "4" == answer(x, y, n)
     x = 2
     y = 3
     n = 4
-    assert "3" = answer(x, y, n)
+    assert "3" == answer(x, y, n)
 
+def solve_one_side(x, n):
+    if x > n:
+        return 0
+    elif x == 1:
+        return factorial(n - 1)
+    elif x == n:
+        return 1
 
+    start = x
+    end = n
+    r = n - x
+    total = 0
+    for i in range(start, end + 1):
+        total += solve_one_side(x - 1, i - 1) * nCr(n - 1, i - 1) * factorial(n - i)
+    return total
 
-test()
+# test()
+def test_one_side():
+    x = 1
+    n = 1
+    # print (solve_one_side(x, n))
+    assert 1 == solve_one_side(x, n)
+    x = 2
+    n = 3
+    assert 3 == solve_one_side(x, n)
+    x = 1
+    n = 5
+    assert 24 == solve_one_side(x, n)
+    x = 5
+    n = 5
+    assert 1 == solve_one_side(x, n)
+    x = 2
+    n = 4
+    assert 11 == solve_one_side(x, n)
+    # _ 4 _ _ = 6 = 3 * 1 * 2
+    # _ _ 4 _ = 3 = 3 * 1 * 1
+    # _ _ _ 4 = 2 = 1 * 2 * 1
+    """
+    1. set max 
+    2. do nCr on # in total choose # on left - (C)
+    3. # of valid combinations on left solve_one_side(x - 1, # on left) - (S)
+    4. # of permuations on left (factorial # on right) - (P)
+    C * S * P
+    """
+
+test_one_side();
+
